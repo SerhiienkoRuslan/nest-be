803b2d604501a7679b3c77fd8b01d87982c6a0d7
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-06-13 18:48:56 +0300

    package.json was updated

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const appOptions = {cors: true};
+  const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
 
@@ -19,8 +19,13 @@ async function bootstrap() {
 
   const port: number = parseInt(`${process.env.PORT}`) || 3000;
 
-  await app.listen(port, () => {
-    console.log("App is running on port " + port);
+  // @ts-ignore
+  await app.listen(port, (err): void => {
+    console.log(`Port: ${port}`);
+
+    if (err) throw err;
+
+    console.log('%c Server running', 'color: green');
   });
 }
 bootstrap();
