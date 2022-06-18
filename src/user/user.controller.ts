31db4b69b0440201df9c649a441d46149b14a90f
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-06-18 20:24:01 +0300

    User API was updated

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Get, Post, Body, Put, Delete, Param, Controller, UsePipes } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Body,
+  Put,
+  Delete,
+  Param,
+  Controller,
+  UsePipes,
+} from '@nestjs/common';
 
 import { ValidationPipe } from '../shared/pipes/validation.pipe';
 
@@ -16,24 +25,32 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('user')
+  @Get('users')
   async findAll(): Promise<UserRO[]> {
     return await this.userService.findAll();
   }
 
+  @Get('me')
+  async findMe(@User('id') id: number): Promise<UserRO> {
+    return await this.userService.findById(id);
+  }
+
   @Get('user')
-  async findMe(@User('email') email: string): Promise<UserRO> {
-    return await this.userService.findByEmail(email);
+  async findUser(@User('id') id: number): Promise<UserRO> {
+    return await this.userService.findById(id);
   }
 
   @Put('user')
-  async update(@User('user') userId: number, @Body('user') userData: UpdateUserDto) {
+  async update(
+    @User('user') userId: number,
+    @Body('user') userData: UpdateUserDto,
+  ) {
     return await this.userService.update(userId, userData);
   }
 
   @UsePipes(new ValidationPipe())
   @Post('user')
-  async create(@Body('user') userData: CreateUserDto) {
+  async create(@Body() userData: CreateUserDto) {
     return this.userService.create(userData);
   }
 
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 
 import { AuthMiddleware } from './auth.middleware';
 import { PrismaService } from '../prisma.service';
@@ -7,21 +12,18 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [
-    UserService,
-    PrismaService
-  ],
+  providers: [UserService, PrismaService],
   controllers: [UserController],
-  exports: [UserService]
+  exports: [UserService],
 })
-
 export class UserModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(AuthMiddleware)
       .forRoutes(
         { path: 'user', method: RequestMethod.GET },
-        { path: 'user', method: RequestMethod.PUT }
+        { path: 'users', method: RequestMethod.GET },
+        { path: 'user', method: RequestMethod.PUT },
       );
   }
 }
