cb03f9791f79b7c9d46e27e5d87a4af1cc087766
RuslanSerhiienko <bruce.t1s@icloud.com>
2022-10-30 13:13:16 +0200

    ConfigModule.forRoot

--- packages/api/src/server.module.ts
+++ packages/api/src/server.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { AppModule } from 'src/app/app.module';
 
 @Module({
   imports: [
     AppModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*']
-    })
+    ConfigModule.forRoot()
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'client'),
+    //   exclude: ['/api*']
+    // })
   ]
 })
 export class ServerModule {}
