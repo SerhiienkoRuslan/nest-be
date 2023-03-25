3e567bb40f5cfaa8deb128454f3010491e54fa22
RuslanSerhiienko <bruce.t1s@icloud.com>
2023-03-25 13:17:37 +0200

    Added pages blog/messager

---
+++ packages/client/pages/blog/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+const BlogPage = () => {
+  return (
+    <StyledPage>
+      Blog
+    </StyledPage>
+  );
+}
+
+export default BlogPage;
---
+++ packages/client/pages/messager/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+const MessagerPage = () => {
+  return (
+    <StyledPage>
+      Messager
+    </StyledPage>
+  );
+}
+
+export default MessagerPage;
