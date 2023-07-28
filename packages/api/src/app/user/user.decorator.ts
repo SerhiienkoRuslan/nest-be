c9542fe7ea95e5934fdb640b8107ca527b152cf6
RuslanSerhiienko <bruce.t1s@icloud.com>
2023-07-28 13:25:03 +0300

    task(user): error handling

--- packages/api/src/app/user/user.decorator.ts
+++ packages/api/src/app/user/user.decorator.ts
@@ -1,20 +1,27 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, HttpStatus } from '@nestjs/common';
 // @ts-ignore
 import * as jwt from 'jsonwebtoken';
 
 import { SECRET } from '../../config';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
 
 export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest();
+  try {
+    const req = ctx.switchToHttp().getRequest();
 
-  // if route is protected, there is a user set in auth.middleware
-  if (!!req.user) return !!data ? req.user[data] : req.user;
+    // if route is protected, there is a user set in auth.middleware
+    if (!!req.user) return !!data ? req.user[data] : req.user;
 
-  // in case a route is not protected, we still want to get the optional auth user from jwt
-  const token = req.headers.authorization ? (req.headers.authorization as string).split(' ') : null;
+    // in case a route is not protected, we still want to get the optional auth user from jwt
+    const token = req.headers.authorization
+      ? (req.headers.authorization as string).split(' ')
+      : null;
 
-  if (token && token[1]) {
-    const decoded: any = jwt.verify(token[1], SECRET);
-    return !!data ? decoded[data] : decoded;
+    if (token && token[1]) {
+      const decoded: any = jwt.verify(token[1], SECRET);
+      return !!data ? decoded[data] : decoded;
+    }
+  } catch (e) {
+    throw new HttpException('USERS.TOKEN_INVALID', HttpStatus.UNAUTHORIZED);
   }
 });
--- packages/api/src/app/user/user.service.ts
+++ packages/api/src/app/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
 
 import { hasPermission } from 'src/shared/pipes/userUtils';
@@ -28,58 +28,82 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async findAll(): Promise<any[]> {
-    return await this.prisma.user.findMany({ select });
+    try {
+      return await this.prisma.user.findMany({ select });
+    } catch (e) {
+      throw new HttpException('USERS.FIND_ALL', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async create(data: CreateUserDto): Promise<UserData> {
-    return await this.prisma.user.create({ data, select });
+    try {
+      return await this.prisma.user.create({ data, select });
+    } catch (e) {
+      throw new HttpException('USERS.CREATE', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async update(userData: UserData, data: UpdateUserDto): Promise<any> {
-    if (hasPermission(userData?.id, data)) {
-      const where = { id: userData?.id };
-      const user = await this.prisma.user.update({ where, data, select });
+    try {
+      if (hasPermission(userData?.id, data)) {
+        const where = { id: userData?.id };
+        const user = await this.prisma.user.update({ where, data, select });
 
-      return { user };
-    } else {
-      throw new HttpException({ error: 'No Permission' }, 403);
+        return { user };
+      } else {
+        throw new HttpException('USERS.UPDATE.FORBIDDEN', HttpStatus.FORBIDDEN);
+      }
+    } catch (e) {
+      throw new HttpException('USERS.UPDATE', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async delete(id: number, user: any): Promise<any> {
-    if (hasPermission(id, user)) {
-      return await this.prisma.user.delete({ where: { id }, select });
-    } else {
-      throw new HttpException({ error: 'No Permission' }, 403);
+    try {
+      if (hasPermission(id, user)) {
+        return await this.prisma.user.delete({ where: { id }, select });
+      } else {
+        throw new HttpException('USERS.DELETE.FORBIDDEN', HttpStatus.FORBIDDEN);
+      }
+    } catch (e) {
+      throw new HttpException('USERS.DELETE', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async findById(id: number): Promise<UserRO | null> {
-    const user = await this.prisma.user.findUnique({
-      where: { id },
-      select: { id: true, ...select },
-    });
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id },
+        select: { id: true, ...select },
+      });
 
-    if (user) {
-      return { user };
+      if (user) {
+        return { user };
+      }
+
+      return null;
+    } catch (e) {
+      throw new HttpException('USERS.FIND_BY_ID', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    return null;
   }
 
   async findByEmail(email: string, params: {} = {}): Promise<UserRO | null> {
-    const user = await this.prisma.user.findUnique({
-      where: { email },
-      select: {
-        ...select,
-        ...params,
-      },
-    });
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email },
+        select: {
+          ...select,
+          ...params,
+        },
+      });
 
-    if (user) {
-      return { user };
+      if (user) {
+        return { user };
+      }
+
+      return null;
+    } catch (e) {
+      throw new HttpException('USERS.FIND_BY_EMAIL', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-    return null;
   }
 }
