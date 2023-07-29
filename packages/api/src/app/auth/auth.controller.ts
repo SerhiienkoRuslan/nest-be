9d67028250e63f347102b59df8d2e1dd3c47f42a
Serhii Demydenko <demydenko.se@gmail.com>
2023-07-29 12:28:58 +0300

    fix(errorsSending): added status and message

--- packages/api/src/app/auth/auth.controller.ts
+++ packages/api/src/app/auth/auth.controller.ts
@@ -47,64 +47,36 @@ export class AuthController {
 
   @Get('verify/:token')
   async verifyEmail(@Param() params: VerifyEmailDto): Promise<IResponse> {
-    try {
-      const isEmailVerified = await this.authService.verifyEmail(params.token);
-      return new ResponseSuccess('LOGIN.EMAIL_VERIFIED', isEmailVerified);
-    } catch (error) {
-      throw new HttpException(
-        error.response || 'LOGIN.ERROR',
-        error.status || HttpStatus.FORBIDDEN,
-      );
-    }
+    const isEmailVerified = await this.authService.verifyEmail(params.token);
+    return new ResponseSuccess('LOGIN.EMAIL_VERIFIED', isEmailVerified);
   }
 
   @Get('resend-verification/:email')
   async sendEmailVerification(@Param() params: ResendEmailDto): Promise<IResponse> {
-    try {
-      await this.authService.createEmailToken(params.email);
-      const isEmailSent = await this.authService.sendEmailVerification(params.email);
+    await this.authService.createEmailToken(params.email);
+    const isEmailSent = await this.authService.sendEmailVerification(params.email);
 
-      if (isEmailSent) {
-        return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
-      } else {
-        throw new HttpException('REGISTRATION.ERROR.MAIL_NOT_SENT', HttpStatus.FORBIDDEN);
-      }
-    } catch (error) {
-      throw new HttpException(
-        error.response || 'LOGIN.ERROR.SEND_EMAIL',
-        error.status || HttpStatus.BAD_GATEWAY,
-      );
+    if (isEmailSent) {
+      return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
+    } else {
+      throw new HttpException('REGISTRATION.ERROR.MAIL_NOT_SENT', HttpStatus.FORBIDDEN);
     }
   }
 
   @Get('forgot-password/:email')
   async sendEmailForgotPassword(@Param() params): Promise<IResponse> {
-    try {
-      const isEmailSent = await this.authService.sendEmailForgotPassword(params.email);
+    const isEmailSent = await this.authService.sendEmailForgotPassword(params.email);
 
-      if (isEmailSent) {
-        return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
-      } else {
-        throw new HttpException('REGISTRATION.ERROR.MAIL_NOT_SENT', HttpStatus.FORBIDDEN);
-      }
-    } catch (error) {
-      throw new HttpException(
-        error.response || 'LOGIN.ERROR.SEND_EMAIL',
-        error.status || HttpStatus.BAD_GATEWAY,
-      );
+    if (isEmailSent) {
+      return new ResponseSuccess('LOGIN.EMAIL_RESENT', null);
+    } else {
+      throw new HttpException('REGISTRATION.ERROR.MAIL_NOT_SENT', HttpStatus.FORBIDDEN);
     }
   }
 
   @Post('reset-password')
   async setNewPassword(@Body() resetPassword: ResetPasswordDto): Promise<IResponse> {
-    try {
-      await this.authService.setNewPassword(resetPassword);
-      return new ResponseSuccess('RESET_PASSWORD.PASSWORD_CHANGED');
-    } catch (error) {
-      throw new HttpException(
-        error.response || 'RESET_PASSWORD.CHANGE_PASSWORD_ERROR',
-        error.status || HttpStatus.BAD_GATEWAY,
-      );
-    }
+    await this.authService.setNewPassword(resetPassword);
+    return new ResponseSuccess('RESET_PASSWORD.PASSWORD_CHANGED');
   }
 }
