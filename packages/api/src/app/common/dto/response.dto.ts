cc59e397af472b7419060bedb048d353ac7e4c55
Serhii Demydenko <demydenko.se@gmail.com>
2023-07-29 12:30:35 +0300

    fix(errorsSending): added status and message

--- packages/api/src/app/common/dto/response.dto.ts
+++ packages/api/src/app/common/dto/response.dto.ts
@@ -1,26 +1,26 @@
 // success: true => message, data
 // success: false => errorMessage, error
 import { IResponse } from '../interfaces/response.interface';
-//
-// export class ResponseError implements IResponse {
-//   constructor(infoMessage: string, data?: any) {
-//     this.success = false;
-//     this.message = infoMessage;
-//     this.data = data;
-//     console.warn(
-//       new Date().toString() +
-//         ' - [Response]: ' +
-//         infoMessage +
-//         (data ? ' - ' + JSON.stringify(data) : ''),
-//     );
-//   }
-//
-//   message: string;
-//   data: any[];
-//   errorMessage: any;
-//   error: any;
-//   success: boolean;
-// }
+
+export class ResponseError implements IResponse {
+  constructor(infoMessage: string, data?: any) {
+    this.success = false;
+    this.message = infoMessage;
+    this.data = data;
+    console.warn(
+      new Date().toString() +
+        ' - [Response]: ' +
+        infoMessage +
+        (data ? ' - ' + JSON.stringify(data) : ''),
+    );
+  }
+
+  message: string;
+  data: any[];
+  errorMessage: any;
+  error: any;
+  success: boolean;
+}
 
 export class ResponseSuccess implements IResponse {
   constructor(infoMessage: string, data?: any, notLog?: boolean) {
